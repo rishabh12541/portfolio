dbd585fd5e92834b9b74217724d64e481dfe2709
Rishabh Bindal <rishabhbindal@Rishabhs-MacBook-Air.local>
2020-12-01 16:56:17 +0530

    opinion
    Signed-off-by: Rishabh Bindal <rishabhbindal@Rishabhs-MacBook-Air.local>

--- server.js
+++ server.js
@@ -9,17 +9,25 @@ const PORT = process.env.PORT || 3456
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 app.use('/' , express.static( __dirname + '/public'))
+try {
+    app.post('/' , async(req , res) => {
+        let fee =  await Feedbs.create( {
+              feedbacks : req.body.feedbacks
+          })
+          res.status(201).send('done')
+      })
+}
+catch(err) {
+    console.error(err)
+}
 
-app.post('/' , async(req , res) => {
-  let fee =  await Feedbs.create( {
-        feedbacks : req.body.feedbacks
-    })
-    res.status(201).send('done')
-})
 
 
 
-db.sync()
+db.sync({force : true})
 .then(() => {
     app.listen(PORT)
+})
+.catch((err) => {
+    console.error(err)
 })
